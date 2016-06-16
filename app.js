334651b2f91599d4da41682073bcc3fd81bfaa5c
Jason Low <lowjiashen@gmail.com>
2016-06-16 14:01:10 +1000

    Added app.js and coffee.js schema

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+var Coffee = require('./models/coffee');
+
+var jason = new Coffee({
+	customer_name: 'Niall',
+	coffee_type: 'Latte',
+	sugar: 3,
+	size: 2
+})
+
+mongoose.connect('mongodb://localhost/test_coffee_app');
+
+// Coffee.create(niall);*
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-	// console.log(request);
-	// console.log(response);
-	// response.send('Hello World!');
-	response.render('index', {
-		title: 'Home',
-		page_description: 'This is a template page made using .ejs files instead of .pug files.',
-    colour: 'cyan'
-	});
+app.get('/', function(req, res) {
+
+	// res.send(niall);
+
+	Coffee.find({}, function(err, coffee) {
+	if (err) return res.send(err);
+	res.send(coffee);
+});
+    // response.render('index', {
+    //     title: 'Home',
+    //     page_description: 'This is a template page made using .ejs files instead of .pug files.',
+    //     colour: 'cyan'
+    // });
+});
+
+app.post('/create', function(request, response) {
+    response.render('index', {
+        title: 'Home',
+        page_description: 'This is a template page made using .ejs files instead of .pug files.',
+        colour: 'cyan'
+    });
 });
 
 app.listen(3000, function() {
-	console.log('Example app listening to post 3000!');
+    console.log('Example app listening to post 3000!');
 });
